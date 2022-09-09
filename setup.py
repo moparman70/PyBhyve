4ffd1a50bcec2101437fb478ee12b751ba91ef49
moparman70 <58913167+moparman70@users.noreply.github.com>
2022-09-09 15:45:15 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,45 @@
+'''
+    PyBhyve
+    ------------
+
+    Communication tool for the B-Hyve water timer device.
+
+
+'''
+import re
+import sys
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+setup(
+    name='PyBhyve',
+    version='0.0.1',
+    url='https://github.com/moparman70/PyBhyve',
+    license='GNU GPL v3',
+    description='Communication tool for the B-Hyve water timer devices',
+    author='',
+    author_email='',
+    maintainer='',
+    maintainer_email='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pybhyvee = pybhyvee.__init__:main'
+        ],
+    },
+)
